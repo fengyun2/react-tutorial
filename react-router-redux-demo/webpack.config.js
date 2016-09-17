aaf5d3882fef40f07945e4342389874bd45b51b1
fengyun2 <2816708306@qq.com>
2016-09-17 12:18:02 +0800

    update webpack.conf

--- react-router-redux-demo/webpack.config.js
+++ react-router-redux-demo/webpack.config.js
@@ -1,3 +1,9 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const precss = require('precss')
+const autoprefixer = require('autoprefixer')
+const T = process.env.npm_lifecycle_event
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
@@ -6,34 +12,93 @@ const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     inject: 'body'
 })
 
-module.exports = {
+const plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    }),
+    HTMLWebpackPluginConfig
+]
+
+let common = {
     entry: [
         `${__dirname}/src/index.js`
     ],
     output: {
         path: `${__dirname}/dist`,
-        filename: 'index_bundle.js'
+        filename: '[name]-[hash].min.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.json', '.scss', '.css'],
+        alias: {
+            ROOT: path.join(__dirname, 'src'),
+            COMPONENTS: path.join(__dirname, 'src/components'),
+            CONTAINERS: path.join(__dirname, 'src/containers'),
+            ACTIONS: path.join(__dirname, 'src/actions'),
+            STORE: path.join(__dirname, 'src/store'),
+            REDUCERS: path.join(__dirname, 'src/reducers'),
+            CONSTANTS: path.join(__dirname, 'src/constants')
+        }
+    },
+    eslint: {
+        configFile: './.eslintrc',
+        failOnWarning: false,
+        failOnError: true
     },
     module: {
         preLoaders: [{
             test: /\.jsx$|\.js$/,
-            loader: 'eslint-loader',
+            loader: 'eslint',
             include: `${__dirname}/src`,
             exclude: /bundle\.js$/
         }],
         loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015', 'react']
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            // {
+            //     test: /\.(css|scss)$/,
+            //     loader: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!sass!postcss'
+            // },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url',
+                query: {
+                    limit: 10000,
+                    name: path.posix.join(__dirname, 'public', '[name].[hash:7].[ext]')
+                }
             }
-        }]
+        ]
     },
+    plugins: plugins,
+    postcss: function() {
+        return [precss, autoprefixer]
+    }
+}
 
-    devServer: {
-        inline: true,
-        port: 8008
-    },
-    plugins: [HTMLWebpackPluginConfig]
+if (T === 'dev' || 'T' === 'start' || !T) {
+    module.exports = merge(common, {
+        devServer: {
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            stats: 'errors-only',
+            host: process.env.HOST || '0.0.0.0',
+            port: process.env.PORT // 8080
+        },
+        devtool: 'eval-source-map',
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    })
+}
+
+if (T === 'build') {
+    module.exports = merge(common, {})
 }
